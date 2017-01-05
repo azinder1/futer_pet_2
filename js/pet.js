d647fc6a5243435a327b71c04f66fd1314f6e122
Adam Zinder and Tyler Ferrin <adam@>
2017-01-05 12:10:20 -0800

    738738738738738738738738738$

--- js/pet.js
+++ js/pet.js
@@ -16,10 +16,13 @@ SearchPet.prototype.pets = function(zipcode, animal, tom) {
         $("div.row.results").append("<div id=" + pet.id.$t + " class='col-sm-4 pet-container'><h3>" + pet.name.$t + "</h3><img class='' src=" + pet.media.photos.photo[3].$t + "></div>");
       }
     });
-    $("div.pet-container").click(function() {
-      var id = $("div.pet-container").attr("id");
+    $("div.pet-container").on("click", function() {
+      console.log(this)
+      var id = $(this).attr("id");
+
+      console.log(id)
       _this.specific(id);
-      $(".results").toggle();
+      $(".results").hide();
     });
   }).fail(function(error) {
     $("div.row.results").append("<h1> Something didn't work, refine your search and try again.</h1>");
@@ -33,7 +36,6 @@ SearchPet.prototype.specific = function(id) {
     url: "http://api.petfinder.com/pet.get?key=" + apiKey + "&format=json&id=" + id,
     type: "GET",
     success: function(response) {
-      console.log(response);
       $('.container .pet').append("<h3>" + response.petfinder.pet.name.$t + "</h3><img class='' src=" + response.petfinder.pet.media.photos.photo[3].$t + ">");
 
     }
